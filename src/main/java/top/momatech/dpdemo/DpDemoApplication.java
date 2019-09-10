603f10e4065ea8e68561b33319f0fa51b0f251e5
ivan <ivandjp@163.com>
2019-09-10 17:53:15 +0800

    init commit

---
+++ src/main/java/top/momatech/dpdemo/DpDemoApplication.java
@@ -0,0 +1,13 @@
+package top.momatech.dpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpDemoApplication.class, args);
+	}
+
+}
